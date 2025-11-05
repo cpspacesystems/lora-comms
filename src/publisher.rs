41d9345daa06da5189b9c660bc04adf9a096edbf
AdamRhoads <adam.rhoads117@gmail.com>
2025-11-04 20:18:12 -0800

    Work

--- src/publisher.rs
+++ src/publisher.rs
@@ -1,7 +1,32 @@
+use zenoh;
 use zenoh::Wait;
+use zenoh::pubsub::Publisher;
+use zenoh::handlers::FifoChannelHandler;
+use zenoh::sample::Sample;
+
+struct pubs<'a> {
+    session : zenoh::Session,
+    publisher : zenoh::pubsub::Publisher<'a>,
+}
+
+impl<'a> pubs<'a> {
+    fn new() -> pubs<'a> {
+        let session = zenoh::open(zenoh::Config::default()).wait().unwrap();
+        let publisher = session.declare_publisher("key/expr").wait().unwrap();
+
+        pubs {
+            session : session, 
+            publisher : publisher
+        }
+    }
+    fn sendStr(&self, s : &str) {
+        self.publisher.put("Hello World").wait().unwrap()
+    }
+}
+
+
 
 fn publisher() {
-    let session = zenoh::open(zenoh::Config::default()).wait().unwrap();
-    let publisher = session.declare_publisher("key/expr").wait().unwrap();
-    publisher.put("Hello World").wait().unwrap();
+    
+    
 }
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -1,10 +1,27 @@
+use zenoh;
 use zenoh::Wait;
+use zenoh::pubsub::Subscriber;
+use zenoh::handlers::FifoChannelHandler;
+use zenoh::sample::Sample;
 
-fn subscriber() {
-    let session = zenoh::open(zenoh::Config::default()).wait().unwrap();
-    let subscriber = session.declare_subscriber("key/expr").wait().unwrap();
-    while let Ok(sample) = subscriber.recv() {
-        println!("Received: {:?}", sample.payload());
+struct subs {
+    session : zenoh::Session,
+    subscriber : zenoh::pubsub::Subscriber<FifoChannelHandler<Sample>>
+}
+
+impl subs {
+    fn new() -> subs {
+        let session = zenoh::open(zenoh::Config::default()).wait().unwrap();
+        let subscriber = session.declare_subscriber("key/expr").wait().unwrap();
+
+        subs {
+            session: session, 
+            subscriber: subscriber
+        }
     }
-    
+    fn get(&self) {
+        while let Ok(sample) = self.subscriber.recv() {
+            println!("Received: {:?}", sample.payload());
+        }
+    }
 }
