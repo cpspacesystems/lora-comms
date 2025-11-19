d52c6b40c4516facec66049be46d04e4ab1b37f8
AdamRhoads <adam.rhoads117@gmail.com>
2025-11-18 16:58:57 -0800

    first 2 fn

--- build.rs
+++ build.rs
@@ -3,5 +3,5 @@
 
 fn main() {
     println!("cargo:rustc-link-search=native=/home/adam/projects/cpss/drivers/lora-comms/src");
-    println!("cargo:rustc-link-lib=static=lr11xx_driver"); // note: no 'lib' prefix, no '.a'
+    println!("cargo:rustc-link-lib=static=lr11xx_driver"); 
 }
