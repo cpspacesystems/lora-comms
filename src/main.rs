f5580fe74ff131aaca60055a79a8ba87ddcafc1c
AdamRhoads <adam.rhoads117@gmail.com>
2026-04-23 07:20:13 +0100

    fixed

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use lr1121wrapper::*;
 const SPI_CHANNEL: u8 = 0;
 const SPI_SPEED: u32 = 500_000;
 const SPI_DEVICE: u8 = 1;
-const GPIO_DEVICE: u8 = 0;
-const PIN_CS: u32 = 18;
-const PIN_INT: u32 = 25;
-const PIN_RST: u32 = 27;
-const PIN_BUSY: u32 = 22;
-const PIN_DIO8: u32 = 24;
+const GPIO_DEVICE: u8 = 4;
+const PIN_CS: u32 = 0xFFFFFFFF;
+const PIN_INT: u32 = 7;
+const PIN_RST: u32 = 5;
+const PIN_BUSY: u32 = 6;
+const PIN_DIO8: u32 = 0xFFFFFFFF;
 
 // Radio
 const FREQUENCY: f32 = 915.0;
@@ -22,7 +22,7 @@ const CODING_RATE: u8 = 5;
 const SYNC_WORD: u8 = 0x12;
 const TX_POWER: i8 = 10;
 const PREAMBLE_LENGTH: u16 = 8;
-const TCXO_VOLTAGE: f32 = 0.0; //1.8 or 0.0???
+const TCXO_VOLTAGE: f32 = 3.3; //1.8 or 0.0???
 
 fn main() {
     unsafe {
