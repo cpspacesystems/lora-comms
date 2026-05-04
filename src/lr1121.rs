d165a7907c6bd55527d2b5dad06671a5de5b046c
AdamRhoads <adam.rhoads117@gmail.com>
2026-05-04 13:27:59 -0700

    units should be changed now

--- src/lr1121.rs
+++ src/lr1121.rs
@@ -2,18 +2,18 @@ use std::{ptr::null, time};
 use crate::{common::{Bandwidth, BufferType, SpreadFactor, LoraCodeRate}, common_config::{UPLINK_TRANSMIT_TIMEOUT_PERIOD, LORA_125KHZ_CH0, LORA_PREAMBLE_LENGTH, INITIAL_CODE_RATE}, errors::{self, AnyError}, lr1121wrapper::{Context, begin, end, getSNR, init, receive, setCodingRate, setFrequency, setSpreadingFactor, transmit}, network::{NetworkRadio, SendError}, packet::{self, OutgoingPacketConfig, PacketMetadata, ReceivedPacket}};
 
 pub struct lr1121Config {
-    pub spi_channel:    u8,
-    pub spi_speed:      u32,
-    pub spi_device:     u8,
-    pub gpio_device:    u8,
-    pub cs:             u32,
-    pub irq:            u32,
-    pub rst:            u32,
-    pub busy:           u32,
-    pub dio8:           u32,
+    pub spi_channel:     u8,
+    pub spi_speed:       u32,
+    pub spi_device:      u8,
+    pub gpio_device:     u8,
+    pub cs:              u32,
+    pub irq:             u32,
+    pub rst:             u32,
+    pub busy:            u32,
+    pub dio8:            u32,
 
-    pub freq:            f32,
-    pub bw:              Bandwidth,
+    pub freq_mhz:        f32,
+    pub bw_khz:          Bandwidth,
     pub sf:              SpreadFactor,
     pub cr:              LoraCodeRate,
     pub sync_word:       u8,
@@ -34,7 +34,7 @@ impl lr1121 {
             spi_channel: 0, spi_speed: 16_000_000, spi_device: 0, 
             gpio_device: 4, cs: 8, irq: 0, 
             rst: 0, busy: 0, dio8: 0, 
-            freq: (LORA_125KHZ_CH0) as f32, bw: BW_125KHZ, sf: SpreadFactor::SF7, 
+            freq_mhz: (LORA_125KHZ_CH0 / 1000000) as f32, bw_khz: BW_125KHZ, sf: SpreadFactor::SF7, 
             cr: LoraCodeRate::CR1, sync_word: 0, power: 22,
             preamble_length: LORA_PREAMBLE_LENGTH, tcxo_voltage: 3.3
         }; 
@@ -79,15 +79,15 @@ impl NetworkRadio for lr1121 {
     fn configure(&mut self) -> Result<(), Self::ConfigureError> {
         let state = unsafe {
             begin(
-                self.ctx,                       //ctx
-                self.config.freq,               //freq MHz
-                self.config.bw,                 //bw
-                self.config.sf,                 //sf
-                self.config.cr,                 //cr
-                self.config.sync_word,          //sync word
-                self.config.power,              //power
-                self.config.preamble_length,    //preamble length
-                self.config.tcxo_voltage        //tcxo voltage
+                self.ctx,                                   //ctx
+                self.config.freq_mhz / 1000000,             //freq MHz
+                lr1121_from_bandwidth(self.config.bw_khz),  //bw
+                self.config.sf,                             //sf
+                lr1121_from_coderate(self.config.cr),       //cr
+                self.config.sync_word,                      //sync word
+                self.config.power,                          //power
+                self.config.preamble_length,                //preamble length
+                self.config.tcxo_voltage                    //tcxo voltage
             )
         };
         if state != 0 {
@@ -174,5 +174,35 @@ impl NetworkRadio for lr1121 {
     /// check if the radio is currently receiving
     fn is_currently_receiving(&mut self) -> Result<bool, AnyError> {
         Ok(self.currentlyRec)
+    }
+
+
+        
+}
+
+pub fn lr1121_from_bandwidth(value: Bandwidth) -> u8 {
+    match value {
+        Bandwidth::Low125khz => bindings_loragw_hal::BW_125KHZ,
+        Bandwidth::Mid250khz => bindings_loragw_hal::BW_250KHZ,
+        Bandwidth::High500khz => bindings_loragw_hal::BW_500KHZ,
+    }
+}
+
+pub fn lr1121_from_coderate(value: LoraCodeRate) -> u8 {
+    match value {
+        LoraCodeRate::CR1 => bindings_loragw_hal::CR_LORA_4_5,
+        LoraCodeRate::CR2 => bindings_loragw_hal::CR_LORA_4_6,
+        LoraCodeRate::CR3 => bindings_loragw_hal::CR_LORA_4_7,
+        LoraCodeRate::CR4 => bindings_loragw_hal::CR_LORA_4_8,
+    }
+}
+
+pub fn lr1121_to_coderate(value: u8) -> Result<LoraCodeRate, errors::InvalidData> {
+    match value {
+        bindings_loragw_hal::CR_LORA_4_5 => Ok(LoraCodeRate::CR1),
+        bindings_loragw_hal::CR_LORA_4_6 => Ok(LoraCodeRate::CR2),
+        bindings_loragw_hal::CR_LORA_4_7 => Ok(LoraCodeRate::CR3),
+        bindings_loragw_hal::CR_LORA_4_8 => Ok(LoraCodeRate::CR4),
+        _ => Err(errors::InvalidData(format!("Invalid data: {value} is not a valid SX1302 lora code rate")))
     }
 }
