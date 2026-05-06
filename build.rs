414fc4d45b6938d5b73f481d83318b613bf8e92b
Alvin-He <zhongjihe.alvin@gmail.com>
2026-05-05 20:29:52 -0700

    Removed uncessary files or files that were moved out already

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=/home/adam/Desktop/LR1121Rust/lora-comms");
-    println!("cargo:rustc-link-lib=static=lora_full");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    println!("cargo:rustc-link-lib=dylib=lgpio");
-}
